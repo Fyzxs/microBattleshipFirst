a35fe66a441fce20653e5720da6a52ff1a1013c5
Fyzxs <Fyzxs@users.noreply.github.com>
2018-02-10 11:34:51 -0800

    initial commit

---
+++ BattleShipMicro/BattleShipTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleShipMicro
+{
+    [TestClass]
+    public class BattleShipTests
+    {
+        [TestMethod]
+        public void AircraftCarrierHorizontal()
+        {
+            //Create an aircraft carrier at zero, zero horizontally
+
+        }
+    }
+}
---
+++ BattleShipMicro/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BattleShipMicro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("BattleShipMicro")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d16dc2de-768e-40aa-9c2f-3b5ad2691393")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

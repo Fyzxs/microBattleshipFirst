48f4a2a85d5ba930276ffdc24d4fbce21f61eb09
Fyzxs <Fyzxs@users.noreply.github.com>
2018-02-10 11:42:03 -0800

    First simple test passing

--- BattleShipMicro/BattleShipTests.cs
+++ BattleShipMicro/BattleShipTests.cs
@@ -1,15 +1,43 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BattleShipMicro
 {
     [TestClass]
+    [UseReporter(typeof(DiffReporter))]
     public class BattleShipTests
     {
         [TestMethod]
         public void AircraftCarrierHorizontal()
         {
             //Create an aircraft carrier at zero, zero horizontally
+            IShip subject = new AircraftCarrier(0, 0, Orientation.Horizontal);
 
+            Approvals.Verify(subject);
         }
     }
+
+    public class Orientation : IOrientation
+    {
+        public static readonly IOrientation Horizontal = new Orientation();
+        private Orientation() { }
+    }
+
+    public class AircraftCarrier : IShip
+    {
+        public AircraftCarrier(int horzCoord, int vertCoord, IOrientation orientation)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "AAAAA";
+        }
+    }
+
+    public interface IOrientation { }
+
+    public interface IShip { }
 }
